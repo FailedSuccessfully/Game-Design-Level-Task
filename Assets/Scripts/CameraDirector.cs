4bcdf95429f9a7ded85d084fddf399a2b66f9b9e
Itay <itay.fichman@gmail.com>
2020-12-06 00:30:56 +0200

    Initial commit

---
+++ Assets/Scripts/CameraDirector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDirector : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+    public float turnSpeed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Rotation();
+        transform.position = player.transform.position + offset;
+        float y = Input.GetAxis("Mouse X") * turnSpeed;
+        transform.LookAt(player.transform.position);
+        //transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + y, 0);
+    }
+
+    void Rotation() {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X")*turnSpeed, Vector3.up) * offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    public float pSpeed = 8f;
+    public float pJump = 7f;
+    public float pRot = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotation();
+        WalkHandler();
+    }
+
+    void WalkHandler() {
+        rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        float pDist = pSpeed * Time.deltaTime;
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(hAxis * pDist, 0f, vAxis * pDist);
+        movement = transform.rotation * movement;
+        Vector3 currPos = transform.position;
+        Vector3 newPos = currPos + movement;
+        rb.MovePosition(newPos);
+    }
+
+    void Rotation() {
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * pRot ,0));
+    }
+}
